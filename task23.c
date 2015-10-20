310a8f3200a87bf3b62d6735af82d773debf4958
Chryssa Terizi <chryssa.terizi@gmail.com>
2015-10-20 23:19:16 +0300

    change

--- task23.c
+++ task23.c
@@ -8,10 +8,11 @@ int main(){
 	
 	//pa = &table[0];
 	//printf("%d\n", pa);
-	value = 0;
+	value = 5;
 	size = sizeof(table) / sizeof(int);
 	pa = search(table, size, value);
-	if(!NULL){
+	printf("%d\n", pa);
+	if(pa != NULL){
 		//printf("%d\n", *pa);
 		//printf("%d\n", pa);
 		printf("To stoixeio %d vrisketai ston pinaka kai h dieythhnsh tou einai %d\n", *pa, pa);
@@ -30,6 +31,7 @@ int * search(int a[], int size, int number){
 	for(i = 0; i < size; i++){
 		if(a[i] == number){
 			pa = pa + i; 
+			printf("%d\n", pa);
 			return pa; 
 		}
 	}
