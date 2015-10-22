0ae7409f6323fdd507a9493847deda541e385af9
Chryssa Terizi <chryssa.terizi@gmail.com>
2015-10-22 22:34:58 +0300

    change the variables in function minmaxavg. Now i call it with array

--- task25.c
+++ task25.c
@@ -1,35 +1,39 @@
 #include <stdio.h>
-double minmaxavg(double *, int, double *, double *);
+double minmaxavg(double [], int, double *, double *);
 
 int main(){
-	double array[] = {3,7,5,12,8,5,0,4,3};
-	double avg, *max, *min;
+	double array[] = {3,7,5,12,8,5,0,4,3,-1};
+	double avg, max, min;
 	int size;
 	
-	max = &array[0];
-	min = &array[0];
 	size = sizeof(array)/sizeof(double);
-	//printf("max : %d\n", max);
-	avg = minmaxavg(array, size, max, min);
+	avg = minmaxavg(array, size, &max, &min);
+	printf("avg = %f, min = %f, max = %f", avg, min, max);
 	return 0;
 }
 
-double minmaxavg(double *array, int num, double *max, double *min){
-	double i;
-
-	//max - min
-	for(i = 0; i <= num; i++){
+double minmaxavg(double array[], int num, double *max, double *min){
+	double avg, sum;
+	int i;
+	
+	*max = array[0];
+	*min = array[0];
+	sum = 0;
+	//max - min - sum
+	for(i = 0; i < num; i++){
 		//max
-		if(*(array + i) > *max){
-			max = array + i;
+		if(array[i] > *max){
+			*max = array[i];
 		}
 		//min
-		if(*(array + i) < *min){
-			min = array + i;
+		if(array[i] < *min){
+			*min = array[i];
 		}
+		//sum
+		sum = sum + array[i];
 
 	}
-	printf("min : %d, max : %d", *min, *max);
-	//max = *array;
-	return 0;
+
+	avg = sum / (double)num;
+	return avg;
 }
