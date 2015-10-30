7b0aa9a4234d1c6ac8c4e2b0a54df2a6add6fa5c
Chryssa Terizi <chryssa.terizi@gmail.com>
2015-10-30 16:57:30 +0200

    small dictionery completed

--- task33.c
+++ task33.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int search(char *[], int **);
-
 
 int main(){
   char str[31] = "start"; 
-  char *lexiko[] = {"associate's degree","bachelor's degree","campus","community college","course","credit","degree","dorm","enroll","exam","faculty","fail","financial aid","fraternity","GPA","graduate","instructor","lecture","major","master's degree","matriculate","notebook","notes","pass","PhD","postgraduate","prerequisite","professor","quiz"};
+  char *lexiko[] = {"associate","bachelor","campus","college","course","credit","degree","dorm","enroll","exam","faculty","fail","fraternity","GPA","graduate","instructor","lecture","major","master","matriculate","notebook","notes","pass","PhD","postgraduate","prerequisite","professor","quiz"};
+  int find, size, i;
   
-  int find;
-  printf("lexiko : %d\n", lexiko);
-  while(strcmp(str, "fin\n") != 0){
-    printf("Give a word :\n");
-    fgets(str, 31, stdin);
-    printf("str before call search = %d\n", str);
-    //printf();
-    find = search(lexiko, &str);
-    //printf("*str = %d\n", *str);
-    //*pointer = &(str[0]);
-    //printf("*pointer = %d\n", *pointer);
+  find = 0;
+  size = sizeof(lexiko) / sizeof(char *);
+  printf("Give a word or \"fin\" to exit : ");
+  while(1){
+    //fgets(str, 31, stdin);
+    scanf("%s", str);
+    if(strcmp(str, "fin") == 0){
+      break;
+    }
+    for(i = 0; i < size; i++){
+      if(strcmp(str, lexiko[i]) == 0){
+        printf("The word : %s exists in dictionary.\n", str);
+        find = 1;
+        break;
+      }
+    }
+    if(find != 1){
+      printf("The word : %s does not exist in dictionary.\n", str);
+    }
+    printf("Give the next word :\n");
   }
   return 0;
-}
-
-int search(char *a[], int **pointer){
-  //*pointer = NULL;
-  int i;
-  char **l;
-  char word[31];
-  
-  i = 0;
-  //*l = &(a);
-  printf("First element in lexiko : %s\n", *a);
-  //printf("*l : %s\n", *l);
-  printf("*pointer : %c\n", *(pointer+1));
-  while(*(pointer+i) != '\0'){
-    word[i] = *(pointer+i);
-    i = i + 1;
-    //*pointer ++;
-  }
-  printf("word : %s\n", word);
-  return 0; 
 }
